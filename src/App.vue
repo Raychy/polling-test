812a3c7007cccaf798860dd4ae8cbc5e524c76eb
Raychy <adesaint1@yahoo.com>
2025-02-24 09:10:36 +0100

    initial commit: polls added

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Poll from './components/Poll.vue'
+</script>
+
+<template>
+  <Poll />
+</template>
---
+++ src/components/Poll.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="poll-container">
+    <div class="poll">
+      <h2>{{ question }}</h2>
+      <section v-if="!hasVoted" class="poll-choices">
+        <button
+          v-for="(choice, index) in choices"
+          :key="index"
+          @click="castVote(index)"
+          class="choice-button"
+        >
+          <span class="checkmark" v-if="selectedIndex === index">✔</span>
+          {{ choice.label }}
+        </button>
+      </section>
+      <section v-else class="poll-results">
+        <div
+          v-for="(choice, index) in choices"
+          :key="index"
+          class="result"
+          :class="{ selected: selectedIndex === index }"
+        >
+          <span class="label">{{ choice.label }}</span>
+          <div class="progress-container">
+            <div
+              class="progress"
+              :style="{ width: (choice.votes / totalVotes) * 100 + '%' }"
+            ></div>
+          </div>
+          <span class="vote-count"
+            >{{ ((choice.votes / totalVotes) * 100).toFixed(0) }}%</span
+          >
+        </div>
+      </section>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const question = ref("Which frontend framework do you prefer?");
+const choices = ref([
+  { label: "Vue", votes: 0 },
+  { label: "React", votes: 0 },
+  { label: "Angular", votes: 0 },
+  { label: "Svelte", votes: 0 },
+]);
+const totalVotes = ref<number>(0);
+const hasVoted = ref(false);
+const selectedIndex = ref<number>();
+
+//  The function `castVote` enables user to cast vote as it accept the selected index
+const castVote = (index: number) => {
+  choices.value[index].votes++;
+  totalVotes.value++;
+  selectedIndex.value = index;
+  hasVoted.value = true;
+};
+</script>
